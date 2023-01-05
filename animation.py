69a89c67adf487b08aadb41b9de112f964108805
usman9856 <67338799+usman9856@users.noreply.github.com>
2023-01-05 15:19:21 +0500

    WebCrawler 2.0 First Commit

---
+++ animation.py
@@ -0,0 +1,36 @@
+import time as tm
+import os
+
+
+def loading():
+    i=1
+    for _ in range (20):
+        if i == 1:
+            i=i+1
+            print ("(/)")
+        elif i == 2:
+            i=i+1
+            print ("(-)")
+        elif i == 3:
+            i=i+1
+            print("(\)")
+        elif i == 4:
+            i=1
+            print ("(-)")
+        tm.sleep(0.1)
+        os.system("cls" if os.name == "nt" else "clear")
+    #tumahri arhi hai
+def loading_line():
+    
+    #print("*************************************************************************************************")
+    for i in range (95):
+        if i==0:
+             print('*|' ,end='')
+        elif i==94:
+            print('|*',end='')
+        else:
+            print('|' ,end='')
+        tm.sleep(0.05)
+    print("\n*************************************************************************************************")
+    
+
